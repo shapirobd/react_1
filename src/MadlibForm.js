5c85ea6536c0d9bfb1806d5fd6cc0a8229e198bd
Brian Shapiro <briandavidshapiro@gmail.com>
2021-01-13 00:56:49 -0500

    form submission only enabled if all text inputs filled

--- src/MadlibForm.js
+++ src/MadlibForm.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useToggle from "./hooks/useToggle";
 import "./MadlibForm.css";
 
 const MadlibForm = ({ handleChange, handleSubmit, formData, isVisible }) => {
+	const [submitBtnEnabled, toggleSubmitBtnEnabled] = useToggle(false);
+
+	// whenever formData is updated, check to see if all form values are filled. if not, keep/make submit button disabled - if so, keep/make submit button enabled
+	useEffect(() => {
+		for (let partOfSpeech in formData) {
+			if (formData[partOfSpeech] === "") {
+				if (submitBtnEnabled) {
+					toggleSubmitBtnEnabled();
+				}
+				return;
+			}
+		}
+		if (!submitBtnEnabled) {
+			toggleSubmitBtnEnabled();
+		}
+	}, [formData]);
+
 	return (
 		<form
 			className="MadlibForm"
@@ -40,7 +58,12 @@ const MadlibForm = ({ handleChange, handleSubmit, formData, isVisible }) => {
 				onChange={handleChange}
 				value={formData.color}
 			></input>
-			<button>Get Story</button>
+			{submitBtnEnabled && <button type="submit">Get Story</button>}
+			{!submitBtnEnabled && (
+				<button type="submit" disabled>
+					Get Story
+				</button>
+			)}
 		</form>
 	);
 };
